07bd20bfe07a1eaaca3b3a2f6ed68854c03265d1
Tim Macdonald <macdonald.tim@gmail.com>
2011-06-23 14:53:52 -0400

    Added support for multi-series polar charts

--- graphing.js
+++ graphing.js
@@ -1,3 +1,29 @@
+Raphael.fn.slice = function ( cx, cy, r, sa, a ) {
+  var x1 = cx + Math.sin(sa) * r;
+  var y1 = cy + Math.cos(sa) * r;
+  
+  var x2 = cx + Math.sin(sa + a) * r;
+  var y2 = cy + Math.cos(sa + a) * r;
+  
+  return this.path("M " + cx + " " + cy + " L " + x1 + " " + y1 + " A" + r + " " + r + " 0 0 0 " + x2 + " " + y2 + " L " + cx + " " + cy);
+};
+
+Raphael.fn.segment = function ( cx, cy, r1, r2, sa, a ) {  
+  var x1 = cx + Math.sin(sa) * r1;
+  var y1 = cy + Math.cos(sa) * r1;
+  
+  var x2 = cx + Math.sin(sa) * r2;
+  var y2 = cy + Math.cos(sa) * r2;
+  
+  var x3 = cx + Math.sin(sa + a) * r2;
+  var y3 = cy + Math.cos(sa + a) * r2;
+  
+  var x4 = cx + Math.sin(sa + a) * r1;
+  var y4 = cy + Math.cos(sa + a) * r1;
+  
+  return this.path("M " + x1 + " " + y1 + " L " + x2 + " " + y2 + " A " + r2 + " " + r2 + " 0 0 0 " + x3 + " " + y3 + " L " + x4 + " " + y4 + " A " + r1 + " " + r1 + " 0 0 1 " + x1 + " " + y1); 
+};
+
 Raphael.fn.dotchart = function(values) {
   function render_series(paper, series_values, offset, max) {
     var width = paper.width / series_values.length;
--- graphing.polarchart.js
+++ graphing.polarchart.js
@@ -1,27 +1,68 @@
 Raphael.fn.polarchart = function( values ) {
-  var colors = ["lightblue","darkgray","lightpink","yellow","blue","green","red","black","gray","orange","red"];
+  var colors = ["lightpink","darkgray","lightblue","yellow","blue","green","red","black","gray","orange","red"];
 
-  var max = Math.max.apply( Math, values );
+  if (this.raphael.is(values[0], "array")) {
 
-  var cx = this.width / 2;
-  var cy = this.height / 2;
-  var radius = Math.min( cx, cy );
+    var max = 0;
+    var total = 0;
+    for ( var i = 0; i < values.length; i++ ) {
+      var current = 0
+      for ( var j = 0; j < values[i].length; j++ ) {
+        current += values[i][j];
+      }
+      total = Math.max( total, current );
+      max = Math.max( max, Math.max.apply( Math, values[i] ) );
+    }
 
-  var offset = 0;
-  for ( var i = 0; i < values.length; i++ ) {
-    var rads = Math.PI * 2 / values.length;
-    
-    var r = radius * values[i] / max;
+    var cx = this.width / 2;
+    var cy = this.height / 2;
+    var radius = Math.min( cx, cy ) / values.length;;
 
-    var x1 = cx + Math.sin( offset ) * r;
-    var y1 = cy + Math.cos( offset ) * r;
+    var offsets = [];
+    for ( var i = 0; i < values[0].length; i++ ) {
+      offsets.push(0);
+    }
 
-    offset += rads;    
+    for ( var i = 0; i < values.length; i++ ) {
+      var rads = Math.PI * 2 / values[i].length;
+      for ( var j = 0; j < values[i].length; j++ ) {
+        var r1 = offsets[j];
+        var r2 = offsets[j] + radius * values[i][j] / max;
 
-    var x2 = cx + Math.sin( offset ) * r;
-    var y2 = cy + Math.cos( offset ) * r;
+        var a1 = j * rads;
+        var a2 = (j + 1) * rads;
 
-    var path = ( "M " + cx + " " + cy + " L " + x1 + " " + y1 + " A " + r + " " + r + " 0 0 0 " + x2 + " " + y2 + " L " + cx + " " + cy );
-    this.path(path).attr({"stroke-width": 2, fill: colors[0], stroke: "#fff"});
+        this.segment( cx, cy, r1, r2, a1, a2 - a1 )
+          .attr({"stroke-width": 0, fill: colors[i]});
+
+        offsets[j] += radius * values[i][j] / max;
+      }
+    }
+
+  } else {
+
+    var max = Math.max.apply( Math, values );
+
+    var cx = this.width / 2;
+    var cy = this.height / 2;
+    var radius = Math.min( cx, cy );
+
+    var offset = 0;
+    for ( var i = 0; i < values.length; i++ ) {
+      var rads = Math.PI * 2 / values.length;
+      
+      var r = radius * values[i] / max;
+
+      var x1 = cx + Math.sin( offset ) * r;
+      var y1 = cy + Math.cos( offset ) * r;
+
+      offset += rads;    
+
+      var x2 = cx + Math.sin( offset ) * r;
+      var y2 = cy + Math.cos( offset ) * r;
+
+      var path = ( "M " + cx + " " + cy + " L " + x1 + " " + y1 + " A " + r + " " + r + " 0 0 0 " + x2 + " " + y2 + " L " + cx + " " + cy );
+      this.path(path).attr({"stroke-width": 2, fill: colors[0], stroke: "#fff"});
+    }
   }
 };
