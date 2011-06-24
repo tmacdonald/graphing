14caa57c54e29aa5f33b4f20c99db0330e7fc7a8
Tim Macdonald <macdonald.tim@gmail.com>
2011-06-24 10:10:44 -0400

    Updated dotchart with offset options

--- graphing.dotchart.js
+++ graphing.dotchart.js
@@ -1,10 +1,17 @@
-Raphael.fn.dotchart = function(values) {
+Raphael.fn.dotchart = function(values, opts) {
+  var opts = opts || {};
+
+  opts.x = opts.x || 0;
+  opts.y = opts.y || 0;
+  opts.width = opts.width || this.width - opts.x;
+  opts.height = opts.height || this.height - opts.y;
+
   function render_series(paper, series_values, offset, max) {
-    var width = paper.width / series_values.length;
+    var width = opts.width / series_values.length;
 
     for ( var i = 0; i < series_values.length; i++ ) {
-      var x = (i * width) + width / 2;
-      var y = offset + width / 2;
+      var x = opts.x + (i * width) + width / 2;
+      var y = opts.y + offset + width / 2;
 
       paper.circle( x, y, series_values[i] / max * width / 2).attr({"fill": "lightblue", "stroke-width": 0});
     }
@@ -16,7 +23,7 @@ Raphael.fn.dotchart = function(values) {
       max = Math.max( max, Math.max.apply( Math, values[i] ) );
     }
     for ( var i = 0; i < values.length; i++ ) {
-      render_series( this, values[i], i * this.height / values.length, max );
+      render_series( this, values[i], i * opts.height / values.length, max );
     }
   } else {
     var max = Math.max.apply( Math, values );
